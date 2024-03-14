7edc82ae55631314e2950bf3fad84ce414ad6b17
jeannelebau@gmail.com <jeannelebau@gmail.com>
2024-03-14 08:58:52 +0100

    save work

--- js/class/pokemon.js
+++ js/class/pokemon.js
@@ -4,7 +4,7 @@ export class Pokemon{
     static allPokemons;
     constructor(id,name,form,attack,defense,stamina){
         this._pokemon_id = id,
-        this._pokemon_name =name, 
+        this._pokemon_name = name, 
         this._form = form,
         this._base_attack = attack, 
         this._base_defense = defense,
@@ -18,8 +18,10 @@ export class Pokemon{
 }
 
 function importPokemon() {
-   pokemon.forEach(poke => {
+
+    pokemon.forEach(poke => {
+        p = new Pokemon(poke.pokemon_id,poke.pokemon_name,poke.form,poke.base_attack,poke.defense,poke.base_stamina);
         
-   })};
+    })};
 
 importPokemon();
