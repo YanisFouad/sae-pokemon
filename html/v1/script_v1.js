f12f5e009a33f840e3c2bf44a077876d3a02fbbf
Zanananane <jeannelebau@gmail.com>
2024-03-22 13:42:53 +0100

    AJout des images dans v1 et tri de forme

--- html/v1/script_v1.js
+++ html/v1/script_v1.js
@@ -5,34 +5,37 @@ import pokemon_types from '../data/pokemon_type.js'
 
 const tbody = document.getElementById("tbody");
 pokemon.forEach(poke => {
-    let tr = document.createElement("tr");
+    if (poke.form == "Normal") {
+        let tr = document.createElement("tr");
 
-    let tdSta = document.createElement("td");
-    tdSta.innerText = poke.base_stamina;
-
-    let tdBa = document.createElement("td");
-    tdBa.innerText = poke.base_attack;
-
-    let tdBd = document.createElement("td");
-    tdBd.innerText = poke.base_defense;
-
-    let tdType = document.createElement("td");
-    tdType.innerText = getType(poke.pokemon_id);
-
-    let tdGen = document.createElement("td");
-    tdGen.innerText = getGen(poke.pokemon_id);
-
-    let tdName = document.createElement("td");
-    tdName.innerText = poke.pokemon_name;
-
-    let tdId = document.createElement("td");
-    tdId.innerText = poke.pokemon_id;
-
-    let tdImg = document.createElement("td");
-    tdImg.innerHTML ="<img src = ../images/"+poke.pokemon_id+".webp> </img>";
-
-    tr.append(tdId, tdName, tdGen, tdType, tdSta, tdBa, tdBd)
-    tbody.appendChild(tr);
+        let tdSta = document.createElement("td");
+        tdSta.innerText = poke.base_stamina;
+    
+        let tdBa = document.createElement("td");
+        tdBa.innerText = poke.base_attack;
+    
+        let tdBd = document.createElement("td");
+        tdBd.innerText = poke.base_defense;
+    
+        let tdType = document.createElement("td");
+        tdType.innerText = getType(poke.pokemon_id);
+    
+        let tdGen = document.createElement("td");
+        tdGen.innerText = getGen(poke.pokemon_id);
+    
+        let tdName = document.createElement("td");
+        tdName.innerText = poke.pokemon_name;
+    
+        let tdId = document.createElement("td");
+        tdId.innerText = poke.pokemon_id;
+    
+        let tdImg = document.createElement("td");
+        tdImg.innerHTML ='<img src = "../webp/images/'+addZero(poke.pokemon_id.toString())+'.webp" width = 15%>  </img>';
+    
+        tr.append(tdId, tdName, tdGen, tdType, tdSta, tdBa, tdBd,tdImg)
+        tbody.appendChild(tr);
+    }
+   
 });
 
 function getGen(id) {
@@ -53,5 +56,19 @@ function getType(id) {
     }
     
 }
+
+function addZero(id) {
+    let idZeros;
+    if (id.length == 1) {
+        idZeros = "00"+id;
+    }else if (id.length == 2){
+        idZeros = "0"+id;
+    }else{
+        idZeros = id;
+    }
+    return idZeros;
+}
+
+console.log(addZero("5"));
 
 getGen(5);
