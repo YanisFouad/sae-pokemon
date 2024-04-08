c7a29357e06a610af8c8318eee3056adefec8517
Yanis Fouad <fouadyanis35@gmail.com>
2024-04-09 01:13:06 +0200

    Add popup function and style

--- html/v3/script_v3.js
+++ html/v3/script_v3.js
@@ -2,14 +2,6 @@ import pokemon from '../data/pokemon.js';
 import generation from '../data/generation.js'
 import pokemon_types from '../data/pokemon_type.js'
 
-let pokemonNormal = Object.values(pokemon).filter((currentPokemon) => {
-    if(currentPokemon.form == "Normal") {
-        return currentPokemon;
-    }
-});
-
-console.log(pokemonNormal);
-
 const urlParams = new URLSearchParams(window.location.search);
 let page = urlParams.get('page');
 let precButton = document.getElementById('prec');
@@ -17,14 +9,32 @@ let suivButton = document.getElementById('suiv');
 let numPage = document.getElementById("page");
 let precLink = precButton.children[0];
 let suivLink = suivButton.children[0];
-let popup = document.getElementById("pop-up");
+const firstIndex = page * 25;
+const lastIndex = firstIndex + 25;
+const tbody = document.getElementById("tbody");
 
+const popup = document.getElementById("pop-up");
+const overlay = document.getElementById('overlay');
+const closePopupButton = document.getElementById('close-popup');
+const popupPokemonName = document.getElementById("popup-pokemon-name");
+const popupPokemonId = document.getElementById("popup-pokemon-id");
+const popupPokemonGeneration = document.getElementById("popup-pokemon-generation");
+const popupPokemonTypes = document.getElementById("popup-pokemon-types");
+const popupPokemonEndurance = document.getElementById("popup-pokemon-endurance");
+const popupPokemonAttaque = document.getElementById("popup-pokemon-attaque");
+const popupPokemonDefence = document.getElementById("popup-pokemon-defence");
+const popupPokemonImage = document.getElementById("popup-pokemon-image")
+
+let pokemonNormal = Object.values(pokemon).filter((currentPokemon) => {
+    if(currentPokemon.form == "Normal") {
+        return currentPokemon;
+    }
+});
 
 if (!page) {
     page = 0;
 }
-const firstIndex = page * 25;
-const lastIndex = firstIndex + 25;
+
 
 if(page <= 0) {
     precButton.hidden = true;
@@ -37,10 +47,8 @@ if(lastIndex + 1 >= pokemonNormal.length) {
     suivButton.hidden = true;
 }
 numPage.innerText = parseInt(page) +1;
-const tbody = document.getElementById("tbody");
-console.log(firstIndex, lastIndex);
+
 for (let i = firstIndex ; i < lastIndex ; i++) {
-    // console.log(i);
         let tr = document.createElement("tr");
 
         let tdSta = document.createElement("td");
@@ -53,10 +61,12 @@ for (let i = firstIndex ; i < lastIndex ; i++) {
         tdBd.innerText = pokemonNormal[i].base_defense;
     
         let tdType = document.createElement("td");
-        tdType.innerText = getType(pokemonNormal[i].pokemon_id);
+        pokemonNormal[i].pokemon_type = getType(pokemonNormal[i].pokemon_id);
+        tdType.innerText = pokemonNormal[i].pokemon_type;
     
         let tdGen = document.createElement("td");
-        tdGen.innerText = getGen(pokemonNormal[i].pokemon_id);
+        pokemonNormal[i].pokemon_generation = getGen(pokemonNormal[i].pokemon_id);
+        tdGen.innerText = pokemonNormal[i].pokemon_generation;
     
         let tdName = document.createElement("td");
         tdName.innerText = pokemonNormal[i].pokemon_name;
@@ -65,14 +75,18 @@ for (let i = firstIndex ; i < lastIndex ; i++) {
         tdId.innerText = pokemonNormal[i].pokemon_id;
     
         let tdImg = document.createElement("td");
-        tdImg.innerHTML ='<img src = "../webp/images/'+addZero(pokemonNormal[i].pokemon_id.toString())+'.webp" width = 15%>  </img>';
+        pokemonNormal[i].pokemon_image = '../webp/images/' + addZero(pokemonNormal[i].pokemon_id.toString()) + '.webp'
+        tdImg.innerHTML = `<img src=${pokemonNormal[i].pokemon_image} width = 15%>`;
     
         tr.append(tdId, tdName, tdGen, tdType, tdSta, tdBa, tdBd,tdImg);
-        tr.addEventListener("click", (e) => pokemonPopup(e));
+        tr.addEventListener("click", (e) => pokemonPopup(pokemonNormal[i], e));
+        tr.pokemonId = tdId;
 
         tbody.appendChild(tr);
 }
 
+closePopupButton.addEventListener("click", () => closePopup());
+overlay.addEventListener("click", () => closePopup());
 
 function getGen(id) {
     for(const[key,gen] of Object.entries(generation)){
@@ -83,6 +97,7 @@ function getGen(id) {
         }
     }
 }
+
 function getType(id) {
     
     for(const[key2,poke] of Object.entries(pokemon_types)){
@@ -105,9 +120,21 @@ function addZero(id) {
     return idZeros;
 }
 
-function pokemonPopup(e) {
-    console.log(e);
-    popup.style.display = "block";
+function pokemonPopup(currentPokemon, e) {
+    popupPokemonImage.src = currentPokemon.pokemon_image;
+    popupPokemonName.innerText = currentPokemon.pokemon_name;
+    popupPokemonId.innerText = currentPokemon.pokemon_id;
+    popupPokemonGeneration.innerText = currentPokemon.pokemon_generation;
+    popupPokemonEndurance.innerText = currentPokemon.base_stamina;
+    popupPokemonDefence.innerText = currentPokemon.base_defense;
+    popupPokemonAttaque.innerText = currentPokemon.base_attack;
+    popupPokemonTypes.innerText = currentPokemon.pokemon_type;
 
+    overlay.classList.add("active")
+    popup.classList.add("active")
 }
 
+function closePopup() {
+    overlay.classList.remove("active");
+    popup.classList.remove("active")
+}
